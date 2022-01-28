cc23681ac6530e1685e58dfcd780e025ed611de0
Matt Boisseau <matt.r.boisseau@gmail.com>
2022-01-27 22:33:41 -0700

    Add JSON GameData singleton

--- LearningMonogame/LearningMonogameOpenGL/Game1.cs
+++ LearningMonogame/LearningMonogameOpenGL/Game1.cs
@@ -4,65 +4,62 @@ using Microsoft.Xna.Framework.Input;
 
 namespace LearningMonogameOpenGL
 {
+	/// <inheritdoc cref="Game" />
 	public class Game1 : Game
 	{
-		readonly GraphicsDeviceManager _graphics;
-		Vector2                        _ballPosition;
-		float                          _ballSpeed;
-		Texture2D                      _ballTexture;
-		SpriteBatch                    _spriteBatch;
-
 		public Game1()
 		{
-			_graphics             = new GraphicsDeviceManager(this);
+			Graphics              = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 			IsMouseVisible        = true;
 		}
 
-		// ReSharper disable once RedundantOverriddenMember
-		protected override void Initialize()
-		{
-			_ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
-			_ballSpeed    = 100f;
+		GraphicsDeviceManager Graphics { get; }
 
-			base.Initialize();
-		}
+		SpriteBatch Batch { get; set; }
+
+		/// <summary>Number of seconds since the last frame. Useful for multiplying into speeds and durations.</summary>
+		float Time { get; set; }
+
+		/// <summary>Current screen position of the cursor.</summary>
+		Vector2 CursorPosition { get; set; }
+
+		/// <summary>The point that the cursor moves toward every frame.</summary>
+		Vector2 CursorTarget { get; set; }
+
+		/// <summary>Texture for the cursor.</summary>
+		Texture2D CursorTexture { get; set; }
+
+		// ReSharper disable once RedundantOverriddenMember
+		protected override void Initialize() { base.Initialize(); }
 
 		protected override void LoadContent()
 		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			Batch = new SpriteBatch(GraphicsDevice);
 
-			_ballTexture = Content.Load<Texture2D>("ball");
+			CursorTexture = Content.Load<Texture2D>("ball");
 		}
 
 		protected override void Update(GameTime gameTime)
 		{
-			if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Time = (float) gameTime.ElapsedGameTime.TotalSeconds;
+
+			var mState = Mouse.GetState();
+
+			if (mState.LeftButton == ButtonState.Pressed)
 			{
-				Exit();
+				CursorTarget = mState.Position.ToVector2();
 			}
 
-			var kState = Keyboard.GetState();
-
-			if (kState.IsKeyDown(Keys.Up))
+			if (Vector2.Distance(CursorPosition, CursorTarget) <= (GameData.Config.CursorSpeed * Time))
 			{
-				_ballPosition.Y -= _ballSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds;
+				CursorPosition = CursorTarget;
 			}
-
-			if (kState.IsKeyDown(Keys.Down))
+			else
 			{
-				_ballPosition.Y += _ballSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds;
-			}
-
-			if (kState.IsKeyDown(Keys.Left))
-			{
-				_ballPosition.X -= _ballSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds;
-			}
-
-			if (kState.IsKeyDown(Keys.Right))
-			{
-				_ballPosition.X += _ballSpeed * (float) gameTime.ElapsedGameTime.TotalSeconds;
+				var temp = CursorTarget - CursorPosition;
+				temp.Normalize();
+				CursorPosition += temp * GameData.Config.CursorSpeed * Time;
 			}
 
 			base.Update(gameTime);
@@ -70,11 +67,21 @@ namespace LearningMonogameOpenGL
 
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+			Graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			_spriteBatch.Begin();
-			_spriteBatch.Draw(_ballTexture, _ballPosition, null, Color.White, 0f, new Vector2(_ballTexture.Width / 2, _ballTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
-			_spriteBatch.End();
+			Batch.Begin();
+			Batch.Draw(
+				CursorTexture,
+				CursorPosition,
+				null,
+				Color.White,
+				0f,
+				new Vector2(CursorTexture.Width / 2f, CursorTexture.Height / 2f),
+				Vector2.One,
+				SpriteEffects.None,
+				0f
+			);
+			Batch.End();
 
 			base.Draw(gameTime);
 		}
---
+++ LearningMonogame/LearningMonogameOpenGL/GameData.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace LearningMonogameOpenGL
+{
+	/// <summary>Static class that stores all JSON data used in the game.</summary>
+	internal static class GameData
+	{
+
+		static GameData()
+		{
+			Config = JsonConvert.DeserializeObject<ConfigStruct>(File.ReadAllText(@"GameData\Config.json"));
+		}
+
+		/// <summary>General configurable game settings.</summary>
+		public static ConfigStruct Config { get; }
+
+		internal struct ConfigStruct
+		{
+			// ReSharper disable once UnassignedGetOnlyAutoProperty
+			/// <summary>Number of pixels the cursor moves per second.</summary>
+			public float CursorSpeed { get; set; }
+		}
+	}
+}
